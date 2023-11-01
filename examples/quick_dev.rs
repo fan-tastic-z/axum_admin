449a2b4eba0f0eb63a0fb1b5dcb2109a616c1882
fan-tastic-z <fantastic.fun.zf@gmail.com>
2023-11-01 11:05:45 +0800

    feat: update examples quick_dev test rpc api

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -16,6 +16,61 @@ async fn main() -> Result<()> {
 	);
 	req_login.await?.print().await?;
 
+	let req_create_task = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "create_task",
+			"params": {
+				"data": {
+					"title": "task AAA"
+				}
+			}
+		}),
+	);
+	let create_task_res = req_create_task.await?;
+	create_task_res.print().await?;
+	let body = create_task_res.json_body()?;
+	let create_task_id = &body["result"]["id"];
+	println!("{:?}", create_task_id);
+
+	let req_update_task = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "update_task",
+			"params": {
+				"id": create_task_id,
+				"data": {
+					"title": "task BB"
+				}
+			}
+		}),
+	);
+	let update_task_res = req_update_task.await?;
+	update_task_res.print().await?;
+
+	let req_delete_task = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "delete_task",
+			"params": {
+				"id": create_task_id
+			}
+		}),
+	);
+	req_delete_task.await?.print().await?;
+
+	let req_list_tasks = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "list_tasks"
+		}),
+	);
+	req_list_tasks.await?.print().await?;
+
 	let req_logoff = hc.do_post(
 		"/api/logoff",
 		json!({
