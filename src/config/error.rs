8735cfce331fb499690015ebeeb08e8beb829c1b
fan-tastic-z <fantastic.fun.zf@gmail.com>
2023-11-02 13:21:26 +0800

    refactor: minor config.rs refactor to config/mod.rs and its error.rs

--- src/config/error.rs
+++ src/config/error.rs
@@ -1,24 +1,12 @@
-use crate::model;
-
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-	// -- Modules
-	Model(model::Error),
+	MissingEnv(&'static str),
+	WrongFormat(&'static str),
 }
 
-// region:    --- Froms
-
-impl From<model::Error> for Error {
-	fn from(val: model::Error) -> Self {
-		Self::Model(val)
-	}
-}
-
-// endregion: --- Froms
-
-// region:    --- Error impl
+// region:    --- Error Boilerplate
 impl core::fmt::Display for Error {
 	fn fmt(
 		&self,
@@ -29,4 +17,4 @@ impl core::fmt::Display for Error {
 }
 
 impl std::error::Error for Error {}
-// endregion: --- Error impl
+// endregion: --- Error Boilerplate
